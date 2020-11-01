2e55ab05c5b338eea7d5009582f669eb96c6ceb4
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-01 22:25:23 +0100

    Add boilerplate for Button component

---
+++ client/src/components/atoms/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'Button.css';
+
+function Button({ children }) {
+  return (
+    <button className="button">
+      { children }
+    </button>
+  );
+}
+
+export default Button;
