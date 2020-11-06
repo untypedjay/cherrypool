550eda3749a143c58afe3fcc80d281fdd2e5f6d6
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-06 22:57:12 +0100

    Add basic structure for Home component

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -1,27 +1,14 @@
-import React, { useState } from 'react';
-import WalletModal from '../organisms/WalletModal';
-import Button from '../atoms/Button';
-import Card from '../organisms/Card';
+import React from 'react';
+import Navbar from '../organisms/Navbar';
 import './Home.css';
 
 function Home() {
-  const [walletModal, setWalletModal] = useState(false);
-
-
   return (
     <div className="home">
-      { walletModal && <WalletModal closeModal={() => setWalletModal(false)}/> }
-      <div className="home__cards">
-        <Card heading="Borrow Crypto" button={<Button onClick={() => setWalletModal(true)}>Borrow</Button>}>
-          <p>Borrow cryptocurrencies and ERC20 tokens without the bureaucracy of a centralized agency.</p>
-        </Card>
-        <Card heading="Exchange Crypto" button={<Button onClick={() => setWalletModal(true)}>Exchange</Button>}>
-          <p>Exchange your cryptocurrencies and ERC20 tokens in a decentralized way.</p>
-        </Card>
-        <Card heading="Stake Crypto" button={<Button onClick={() => setWalletModal(true)}>Stake</Button>}>
-          <p>Do more with your crypto assets. Invest them and earn passive income.</p>
-        </Card>
-      </div>
+      <Navbar/>
+      <main className="home__main">
+
+      </main>
     </div>
   );
 }
---
+++ client/src/components/organisms/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Borrow</li>
+        <li>Exchange</li>
+        <li>Stake</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
