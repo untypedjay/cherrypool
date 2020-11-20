6afc803460e87f111b70a07c875c034870484b9f
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-20 22:17:14 +0100

    Implement menu functionality

--- client/src/components/organisms/Menu.js
+++ client/src/components/organisms/Menu.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FaHandHoldingUsd, FaExchangeAlt, FaPiggyBank } from 'react-icons/fa';
 import Logo from '../../images/logo-small.png';
 import '../../styles/Menu.css';
 
-function Menu({ section, setSection }) {
+function Menu({ selected }) {
   const iconSize = '35px';
+  const history = useHistory();
+
+  const calculateStyling = section => {
+    if (section == selected) {
+      return 'menu__item menu__item--selected';
+    }
+    return 'menu__item';
+  };
 
   return (
     <div className="menu">
       <Link to="/"><img className="menu__logo" src={Logo} alt="Cherry Pool"/></Link>
-      <button className="menu__item" onClick={() => setSection('borrow')}>
+      <button className={calculateStyling('/borrow')} onClick={() => history.push('/borrow')}>
         <FaHandHoldingUsd className="menu__icon" size={iconSize}/>
         <p className="menu__item-text">Borrow</p>
       </button>
-      <button className="menu__item" onClick={() => setSection('exchange')}>
+      <button className={calculateStyling('/exchange')} onClick={() => history.push('/exchange')}>
         <FaExchangeAlt className="menu__icon" size={iconSize}/>
         <p className="menu__item-text">Exchange</p>
       </button>
-      <button className="menu__item" onClick={() => setSection('stake')}>
+      <button className={calculateStyling('/stake')} onClick={() => history.push('/stake')}>
         <FaPiggyBank className="menu__icon" size={iconSize}/>
         <p className="menu__item-text">Stake</p>
       </button>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import '../../styles/Home.css';
 import WalletModal from '../organisms/WalletModal';
 import Menu from '../organisms/Menu';
-import NoWallet from "../molecules/NoWallet";
+import NoWallet from '../molecules/NoWallet';
 
 function Home() {
   const [walletModal, setWalletModal] = useState(false);
   const [account, setAccount] = useState('');
+  const location = useLocation();
 
   return (
     <div className="home">
       { walletModal && <WalletModal close={() => setWalletModal(false)}/> }
-      <Menu/>
+      <Menu selected={location.pathname}/>
       <main className="home__main">
         { !account && <NoWallet onClick={() => setWalletModal(true)}/> }
       </main>
