cd5eb77bbb3e135c6e0ab3f5bf227edfa24c56e1
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-21 23:56:30 +0100

    Improve CherryToken! !

--- contracts/CherryToken.sol
+++ contracts/CherryToken.sol
@@ -19,8 +19,8 @@ contract CherryToken {
   string public constant symbol = "CTN";
   uint8 public constant decimals;
 
-  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-  event Transfer(address indexed from, address indexed to, uint tokens);
+  event Approval(address indexed owner, address indexed spender, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
 
   mapping(address => uint256) balances;
   mapping(address => mapping(address => uint256)) allowed;
@@ -40,7 +40,7 @@ contract CherryToken {
     return balances[tokenOwner];
   }
 
-  function transfer(address receiver, uint numTokens) public returns (bool) {
+  function transfer(address receiver, uint numTokens) public returns (bool success) {
     require(numTokens <= balances[msg.sender]);
     balances[msg.sender] = balances[msg.sender].sub(numTokens);
     balances[receiver] = balances[receiver].add(numTokens);
@@ -48,9 +48,9 @@ contract CherryToken {
     return true;
   }
 
-  function approve(address delegate, uint numTokens) public returns (bool) {
-    allowed[msg.sender][delegate] = numTokens;
-    emit Approval(msg.sender, delegate, numTokens);
+  function approve(address delegate, uint value) public returns (bool) {
+    allowed[msg.sender][delegate] = value;
+    emit Approval(msg.sender, delegate, value);
     return true;
   }
 
@@ -58,13 +58,13 @@ contract CherryToken {
     return allowed[owner][delegate];
   }
 
-  function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-    require(numTokens <= balances[owner]);
-    require(numTokens <= allowed[owner][msg.sender]);
-    balances[owner] = balances[owner].sub(numTokens);
-    allowed[owner][msg.sender] = allowed[from][msg.sender].sub(numTokens);
-    balances[buyer] = balances[buyer].add(numTokens);
-    Transfer(owner, buyer, numTokens);
+  function transferFrom(address from, address to, uint value) public returns (bool success) {
+    require(value <= balances[from]);
+    require(value <= allowed[owner][msg.sender]);
+    balances[from] = balances[from].sub(value);
+    allowed[from][msg.sender] = allowed[from][msg.sender].sub(value);
+    balances[to] = balances[to].add(value);
+    Transfer(from, to, value);
     return true;
   }
 }
