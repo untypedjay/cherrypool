0f3181c76ed78521dd623a3118f6394403f10c63
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-22 13:51:23 +0100

    Add AccountBalance

---
+++ client/src/components/molecules/AccountBalance.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useWeb3 } from '../../context/Web3Context';
+import { useAccount } from '../../context/AccountContext';
+import '../../styles/AccountBalance.css';
+
+
+function AccountBalance() {
+  const web3 = useWeb3();
+  const account = useAccount();
+  const [etherBalance, setEtherBalance] = useState(0);
+  console.log(web3)
+
+  useEffect(() => {
+    web3.eth.getBalance(account, (err, balance) => {
+      setEtherBalance(web3.utils.fromWei(balance, 'ether'));
+    });
+  }, []);
+
+  return (
+    <div className="account-balance">
+      <p>{ etherBalance } ETH</p>
+    </div>
+  );
+}
+
+export default AccountBalance;
