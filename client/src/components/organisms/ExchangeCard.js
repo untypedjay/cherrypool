ecf188a6b56d624d642b92f4201b3d10e3a9e499
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-11-22 13:52:58 +0100

    Add ExchangeCard

---
+++ client/src/components/organisms/ExchangeCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../styles/ExchangeCard.css';
+
+function ExchangeCard() {
+  return (
+    <div className="exchange-card">
+
+    </div>
+  );
+}
+
+export default ExchangeCard;
