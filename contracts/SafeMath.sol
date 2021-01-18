8602cc5a595d37e62cfc19d9e4a75a98e70c7def
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-18 22:20:16 +0100

    Extract SafeMath into separate file

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,33 @@
+library SafeMath {
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0, "SafeMath: division by zero");
+    uint256 c = a / b;
+
+    return c;
+  }
+}
