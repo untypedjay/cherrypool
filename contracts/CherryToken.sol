757ac868e7d3b85e9926cd32e32aa33be1b5b003
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-18 15:28:38 +0100

    Add multiplication function

--- contracts/CherryToken.sol
+++ contracts/CherryToken.sol
@@ -11,6 +11,20 @@ library SafeMath {
     require(c >= a);
     return c;
   }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
 }
 
 contract CherryToken {
