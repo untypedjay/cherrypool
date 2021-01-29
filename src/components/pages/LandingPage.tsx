8b118841e2b86c1bf5efc10b3576964c7be35f26
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-29 10:55:45 +0100

    Update button descriptions

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -26,13 +26,13 @@ function LandingPage() {
       </div>
 
       <div className="landing-page__services" ref={serviceRef}>
-        <Card heading="Liquidity Pool" button={<PrimaryButton onClick={() => history.push('/liquidity')}>Borrow</PrimaryButton>}>
+        <Card heading="Liquidity Pool" button={<PrimaryButton onClick={() => history.push('/liquidity')}>Provide Liquidty</PrimaryButton>}>
           <p className="landing-page__paragraph--card">Provide liquidity and earn passive income.</p>
         </Card>
-        <Card heading="DEX" button={<PrimaryButton onClick={() => history.push('/swap')}>Exchange</PrimaryButton>}>
+        <Card heading="DEX" button={<PrimaryButton onClick={() => history.push('/swap')}>Swap</PrimaryButton>}>
           <p className="landing-page__paragraph--card">Exchange your Ether for CherryTokens and vice versa completely decentralized.</p>
         </Card>
-        <Card heading="Token Faucet" button={<PrimaryButton onClick={() => history.push('/faucet')}>Stake</PrimaryButton>}>
+        <Card heading="Token Faucet" button={<PrimaryButton onClick={() => history.push('/faucet')}>Request Tokens</PrimaryButton>}>
           <p className="landing-page__paragraph--card">Request CherryTokens from the faucet for testing purposes.</p>
         </Card>
       </div>
